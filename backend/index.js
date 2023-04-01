ecb93c6cf4e9ad2fee491fa20d15795deb815f26
kumarpp9718@gmail.com <kumarpp9718@gmail.com>
2023-04-01 21:03:41 +0530

    Changes made fir deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 5000 || process.env.PORT
 require('dotenv').config()
 const mongoDB = require('./db')
 mongoDB()
